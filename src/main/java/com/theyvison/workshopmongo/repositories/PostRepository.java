7a175653f6ad4364cf1c045a3adc70a5ed98cecf
Theyvison <theyvison.jf@hotmail.com>
2025-09-11 15:25:45 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/theyvison/workshopmongo/repositories/PostRepository.java
@@ -0,0 +1,10 @@
+package com.theyvison.workshopmongo.repositories;
+
+import com.theyvison.workshopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
