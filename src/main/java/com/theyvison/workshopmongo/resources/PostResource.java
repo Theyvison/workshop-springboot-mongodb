a6d6e11e63b7b3e1262b2bff4dab893ab121222d
Theyvison <theyvison.jf@hotmail.com>
2025-09-12 15:12:08 -0300

    Consulta simples com query methods

--- src/main/java/com/theyvison/workshopmongo/resources/PostResource.java
+++ src/main/java/com/theyvison/workshopmongo/resources/PostResource.java
@@ -1,10 +1,13 @@
 package com.theyvison.workshopmongo.resources;
 
 import com.theyvison.workshopmongo.domain.Post;
+import com.theyvison.workshopmongo.resources.utils.URL;
 import com.theyvison.workshopmongo.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -17,4 +20,12 @@ public class PostResource {
         Post obj = postService.findById(id);
         return ResponseEntity.ok().body(obj);
     }
+
+    @GetMapping(value = "/titlesearch")
+    public ResponseEntity<List<Post>> findByTile(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> posts = postService.findByTitle(text);
+        return ResponseEntity.ok().body(posts);
+    }
+
 }
